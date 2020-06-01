d50b385c7690abb82f2216e2ba80935e0a6c6063
System Administrator <root@deva-p.lan>
2020-06-01 10:59:24 -0500

    Updates

---
+++ precache-manifest.ccd7ff0847f02e60d1edf780dce3a2c1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2952f0503a3702bfd52c806f34b6ce76",
+    "url": "/react-helloworld/index.html"
+  },
+  {
+    "revision": "9440ddbaade06aa9baf8",
+    "url": "/react-helloworld/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "180e8e6c87d62e583e41",
+    "url": "/react-helloworld/static/js/2.81d000b4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-helloworld/static/js/2.81d000b4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9440ddbaade06aa9baf8",
+    "url": "/react-helloworld/static/js/main.fcc46ddf.chunk.js"
+  },
+  {
+    "revision": "6c7bd8daed57c5d389e3",
+    "url": "/react-helloworld/static/js/runtime-main.a2de7bcc.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-helloworld/static/media/logo.5d5d9eef.svg"
+  }
+]);
